1748c497c174651d1d3fab8bc10889ffc3f5fb4f
Antonio Moreira <antoniomoreira@pecege.com>
2025-04-23 18:48:25 -0300

    muitas coisas

--- pos-api/pos_api/app.py
+++ pos-api/pos_api/app.py
@@ -1,22 +1,26 @@
 import uvicorn
+from rich import print
+from starlette.routing import Route
+from starlette.requests import Request
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
-from starlette.routing import Route
+
+from .text_processing import get_pos_tags
 
 async def homepage(request) -> JSONResponse:
     return JSONResponse({
         "message": "Welcome to the API!"
     })
 
-
-async def handle_post(request) -> JSONResponse:
+async def handle_tagger(request:Request) -> JSONResponse:
+    assert isinstance(request, Request)
+    print(request)
+    print(type(request))
     try:
         data = await request.json()
-        # Process the data as needed
-        return JSONResponse({
-            "message": "Data received successfully",
-            "data": data
-        }, status_code=201)
+        assert isinstance(data, dict) and "sentence" in data
+        tags = get_pos_tags(data["sentence"])
+        return JSONResponse(tags, status_code=201)
     except Exception as e:
         return JSONResponse({
             "error": str(e)
@@ -32,8 +36,8 @@ async def health_check(request) -> JSONResponse:
 routes = [
     Route("/", endpoint=homepage),
     Route("/health", endpoint=health_check),
+    Route("/tagger", endpoint=handle_tagger, methods=["POST"]),
 ]
-
 
 app = Starlette(debug=True, routes=routes)
 
--- pos-api/pos_api/text_processing.py
+++ pos-api/pos_api/text_processing.py
@@ -1,12 +1,17 @@
+import subprocess
+
 import spacy 
 
-nlp = spacy.load("pt_core_news_sm")
+try:
+    nlp = spacy.load("pt_core_news_sm")
+except OSError:
+    subprocess.run(["python3", "-m", "spacy", "download", "pt_core_news_sm"])
+    nlp = spacy.load("pt_core_news_sm")
 
-def get_pos_tags(text) -> list[tuple[str, str]]:
+def get_pos_tags(text) -> dict[str, str]:
     """
     Process the text and return a list of tuples with the word and its part of speech tag.
     """
     doc = nlp(text)
-    pos_tags = [(token.text, token.pos_) for token in doc]
+    pos_tags = {token.text:token.pos_ for token in doc}
     return pos_tags
-
