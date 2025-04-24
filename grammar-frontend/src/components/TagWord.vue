7f271a7725ccf89700012c62cad33145498cd45d
Antonio Moreira <antoniomoreira@pecege.com>
2025-04-23 21:26:44 -0300

    tagword.vue text_processing.py

---
+++ grammar-frontend/src/components/TagWord.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const { word, tag } = defineProps<{
+  word: string;
+  tag?: string;
+  correct: string;
+}>();
+
+defineEmits<{
+  clear: [void];
+  startTagging: [void];
+}>();
+</script>
+
+<template>
+  <div class="tag-word">
+    <span class="tag is-info is-size-3" @click="$emit('startTagging')">{{
+      word
+    }}</span>
+    <span
+      class="tag is-size-5"
+      :class="[tag === correct ? 'is-success' : 'is-danger']"
+      @click="$emit('clear')"
+      >{{ tag }}</span
+    >
+  </div>
+</template>
+
+<style scoped>
+.tag-word {
+  display: flex;
+  align-items: center;
+  margin: 0.5rem;
+  flex-direction: column;
+  gap: 0.5rem 0.2rem;
+}
+
+.tag-word .tag:empty {
+  opacity: 0;
+}
+</style>
--- pos-api/pos_api/text_processing.py
+++ pos-api/pos_api/text_processing.py
@@ -8,6 +8,7 @@ except OSError:
     subprocess.run(["python3", "-m", "spacy", "download", "pt_core_news_lg"])
     nlp = spacy.load("pt_core_news_lg")
 
+
 def get_pos_tags(text) -> list[dict[str, str]]:
     """
     Process the text and return a list of tuples with the word and its part of speech tag.
