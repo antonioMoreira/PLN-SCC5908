1aea9e9775d194ebe4d03642eb6669dbe40f623c
Daniel Bertazzo <danielpcb610@gmail.com>
2025-04-27 16:35:16 +0000

    Tradução das tags.

--- pos-api/pos_api/text_processing.py
+++ pos-api/pos_api/text_processing.py
@@ -8,11 +8,34 @@ except OSError:
     subprocess.run(["python3", "-m", "spacy", "download", "pt_core_news_lg"])
     nlp = spacy.load("pt_core_news_lg")
 
+upos_translation = {
+    "ADJ"  : "Adjetivo",
+    "ADP"  : "Preposição",
+    "ADV"  : "Advérbio",
+    "AUX"  : "Verbo Auxiliar",
+    "CCONJ": "Conjunção Coordenativa",
+    "DET"  : "Determinante (art ou pron não nominal)",
+    "INTJ" : "Interjeição",
+    "NOUN" : "Substantivo",
+    "NUM"  : "Numeral",
+    "PART" : "Partícula",
+    "PRON" : "Pronome",
+    "PROPN": "Nome Próprio",
+    "PUNCT": "Pontuação",
+    "SCONJ": "Conjunção Subordinativa",
+    "SYM"  : "Símbolo",
+    "VERB" : "Verbo",
+    "X"    : "Outro"
+}
 
 def get_pos_tags(text) -> list[dict[str, str]]:
     """
-    Process the text and return a list of tuples with the word and its part of speech tag.
+    Processa o texto e retorna uma lista de dicionários com a palavra e sua classe gramatical em português.
     """
     doc = nlp(text)
-    pos_tags = [{'word': token.text, 'tag':token.pos_} for token in doc]
+    pos_tags = []
+    for token in doc:
+        tag = token.pos_
+        translated_tag = upos_translation.get(tag, tag)  # Se não achar, usa a própria tag original
+        pos_tags.append({'word': token.text, 'tag': translated_tag})
     return pos_tags
