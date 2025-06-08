c15d9cab562b9446220495a0f34528cdd33e96ee
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-06-07 22:37:46 -0300

    feat: new taggin and dnd

--- grammar-frontend/src/components/TagWord.vue
+++ grammar-frontend/src/components/TagWord.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-const { word, tag } = defineProps<{
+const { word, tag, isTagged } = defineProps<{
   word: string;
   tag?: string;
-  correct: string;
+  isTagged: boolean;
 }>();
 
 defineEmits<{
@@ -13,28 +13,36 @@ defineEmits<{
 
 <template>
   <div class="tag-word">
-    <span class="tag is-info is-size-3" @click="$emit('startTagging')">{{
+    <span class="tag is-info" @click="isTagged || $emit('startTagging')">{{
       word
     }}</span>
     <span
-      class="tag is-size-5"
-      :class="[tag === correct ? 'is-success' : 'is-danger']"
-      @click="$emit('clear')"
+      class="tag tag-class is-success"
+      @click="isTagged || $emit('clear')"
       >{{ tag }}</span
     >
   </div>
 </template>
 
 <style scoped>
+.tag {
+  font-size: clamp(1.2rem, 1.0196rem + 0.9022vw, 1.7188rem);
+}
+
 .tag-word {
   display: flex;
   align-items: center;
   margin: 0.5rem;
   flex-direction: column;
   gap: 0.5rem 0.2rem;
+  min-width: clamp(3rem, 1.6957rem + 6.5217vw, 6.75rem);
 }
 
 .tag-word .tag:empty {
   opacity: 0;
+}
+
+.tag-class {
+  font-size: clamp(0.8333rem, 0.7406rem + 0.4638vw, 1.1rem);
 }
 </style>
